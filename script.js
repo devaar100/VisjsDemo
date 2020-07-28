1d3bb5d3e24b339c485bec344543d636f9267f69
Aarnav Jindal <developer.aarnav100@gmail.com>
2020-07-28 17:53:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
  * Created by aarnavjindal on 19/03/20.
  */
 
+// https://visjs.org/
+
 onload = function () {
+
     // create a network
     const container = document.getElementById('container');
     const genNew = document.getElementById('generate-graph');
+
     // initialise graph options
     const options = {
         edges: {
@@ -28,6 +32,7 @@ onload = function () {
             }
         }
     };
+
     // initialize your network!
     const network = new vis.Network(container);
     network.setOptions(options);
@@ -35,18 +40,26 @@ onload = function () {
     function createData(){
         const cities = ['Delhi', 'Mumbai', 'Gujarat', 'Goa', 'Kanpur', 'Jammu', 'Hyderabad', 'Bangalore', 'Gangtok', 'Meghalaya'];
 
+        // Initialising number of nodes in graoh dynamically
         const V = Math.floor(Math.random() * cities.length) + 3;
+
+        // Preparing node data for Vis.js
         let vertices = [];
         for(let i=0;i<V;i++){
             vertices.push({id:i, label: cities[i-1]});
         }
 
+        // Preparing edges for Vis.js
         let edges = [];
         for(let i=1;i<V;i++){
+            // Picking a neighbour from 0 to i-1 to make edge to
             let neigh = Math.floor(Math.random()*i);
+
+            // Adding the edge between node and neighbour
             edges.push({from: i, to: neigh, color: 'orange',label: String(Math.floor(Math.random()*70)+30)});
         }
 
+        //Preparing data object for Vis.js
         const data = {
             nodes: vertices,
             edges: edges
@@ -55,6 +68,7 @@ onload = function () {
     }
 
     genNew.onclick = function () {
+        // Creating and setting data to network
         let data = createData();
         network.setData(data);
     };
